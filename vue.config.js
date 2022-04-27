ec1f688a2ba22707b16dea2b00cc2fdf5a4edade
byp0409 <1024567199@qq.com>
2022-04-27 23:26:09 +0800

    代理服务器

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,19 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  // 配置代理服务器
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8090', //要跨域的域名
+        changeOrigin: true, //开启跨域
+        pathRewirte: {
+          //重写路径
+          '^/api': '/api',
+        },
+      },
+    },
+  },
 });
