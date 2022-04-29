8afe7a45c88f681055fbbe8e582ff1804efba70e
byp0409 <1024567199@qq.com>
2022-04-29 23:54:59 +0800

    顶部导航

--- src/components/TopNav/TopNav.vue
+++ src/components/TopNav/TopNav.vue
@@ -2,6 +2,25 @@
   <div class="topNav">
     <i @click="change" v-show="!flag" class="el-icon-s-fold"></i>
     <i @click="change" v-show="flag" class="el-icon-s-unfold"></i>
+    <div class="right">
+      <!-- 多语言选择 -->
+      <div class="lang">
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            多语言
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>中文</el-dropdown-item>
+            <el-dropdown-item>English</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+      <div class="user">
+        <span>欢迎用户：</span>
+      </div>
+      <div class="logout">退出登录</div>
+    </div>
   </div>
 </template>
 
@@ -33,6 +52,33 @@ export default {
   i {
     margin-top: 12px;
     font-size: 32px;
+  }
+  .right {
+    display: flex;
+    justify-content: space-between;
+    float: right;
+    width: 210px;
+    margin-right: 20px;
+    line-height: 50px;
+    .lang {
+      .el-dropdown-link {
+        cursor: pointer;
+        color: #fff;
+      }
+      .el-icon-arrow-down {
+        font-size: 12px;
+      }
+    }
+    .user {
+      color: #fff;
+      font-size: 14px;
+      cursor: default;
+    }
+    .logout {
+      color: #fff;
+      font-size: 14px;
+      cursor: pointer;
+    }
   }
 }
 </style>
