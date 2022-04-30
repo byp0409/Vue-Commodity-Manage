c60f53fa5489bba03b7a3ad7ae63b661849379e4
byp0409 <1024567199@qq.com>
2022-04-30 23:17:37 +0800

    顶部导航

--- src/components/TopNav/TopNav.vue
+++ src/components/TopNav/TopNav.vue
@@ -5,21 +5,21 @@
     <div class="right">
       <!-- 多语言选择 -->
       <div class="lang">
-        <el-dropdown>
+        <el-dropdown @command="changeLang">
           <span class="el-dropdown-link">
-            多语言
-            <i class="el-icon-arrow-down el-icon--right"></i>
+            {{ $t('topNav.multi') }}
           </span>
+          <i class="el-icon-arrow-down el-icon--right" style="color: #fff"></i>
           <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>中文</el-dropdown-item>
-            <el-dropdown-item>English</el-dropdown-item>
+            <el-dropdown-item command="zh">中文</el-dropdown-item>
+            <el-dropdown-item command="en">English</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
       </div>
       <div class="user">
-        <span>欢迎用户：</span>
+        <span>{{ $t('topNav.wel') }}{{ username }}</span>
       </div>
-      <div class="logout">退出登录</div>
+      <div class="logout" @click="logout">{{ $t('topNav.logout') }}</div>
     </div>
   </div>
 </template>
@@ -32,9 +32,26 @@ export default {
     return {
       // 控制导航伸缩
       flag: false,
+      username: '',
     };
   },
+  mounted() {
+    // 存储一下用户名
+    this.username = localStorage.getItem('username');
+  },
   methods: {
+    // 退出登录
+    logout() {
+      // 没有退出登录的接口，只销毁token
+      localStorage.removeItem('manage_token');
+      localStorage.removeItem('username');
+      this.$router.replace('/login');
+    },
+    // 切换语言
+    changeLang(command) {
+      // 切换语言模式
+      this.$i18n.locale = command;
+    },
     // 切换导航栏状态
     change() {
       this.flag = !this.flag;
@@ -55,12 +72,13 @@ export default {
   }
   .right {
     display: flex;
-    justify-content: space-between;
+    justify-content: flex-end;
     float: right;
-    width: 210px;
+    // width: 200;
     margin-right: 20px;
     line-height: 50px;
     .lang {
+      margin-right: 30px;
       .el-dropdown-link {
         cursor: pointer;
         color: #fff;
@@ -70,11 +88,13 @@ export default {
       }
     }
     .user {
+      margin-right: 30px;
       color: #fff;
       font-size: 14px;
       cursor: default;
     }
     .logout {
+      margin-right: 15px;
       color: #fff;
       font-size: 14px;
       cursor: pointer;
